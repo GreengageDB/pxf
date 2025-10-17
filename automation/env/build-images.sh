4f8e775bfb2a58b21c8a5a4c9530979c1ad62fbc
Maxim Gajdaj <maxim.gajdaj@gmail.com>
2025-10-17 12:08:38 +0300

    Add greengagedb/ prefix to docker images

--- automation/env/build-images.sh
+++ automation/env/build-images.sh
@@ -12,18 +12,18 @@ echo "===================================="
 echo "      Build Hadoop 3.3.6 image      "
 echo "===================================="
 pushd hadoop
-docker build -f Dockerfile -t pxf-hadoop:3.3.6 .
+docker build -f Dockerfile -t greengagedb/pxf-hadoop:3.3.6 .
 popd
 
 #echo "===================================="
 #echo "      Build Vault image      "
 #echo "===================================="
-docker build -f ./vault/Dockerfile -t pxf-vault-test:it .
+docker build -f ./vault/Dockerfile -t greengagedb/pxf-vault-test .
 
 echo "=============================="
 echo "Build PXF image for automation"
 echo "=============================="
 # GGDB_IMAGE is from https://github.com/GreengageDB/greengage/tree/main/ci
 pushd ../..
-docker build -t ggdb6_pxf_automation:it --build-arg "GGDB_IMAGE=${GGDB_IMAGE:-greengagedb/ggdb6_ubuntu:6.29.0}" -f automation/env/Dockerfile .
+docker build -t greengagedb/ggdb6_pxf_automation --build-arg "GGDB_IMAGE=${GGDB_IMAGE:-greengagedb/ggdb6_ubuntu:6.29.0}" -f automation/env/Dockerfile .
 popd
