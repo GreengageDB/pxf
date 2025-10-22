93264cad1c20536032da00ef6bba7db7559d50bd
Maxim Gajdaj <maxim.gajdaj@gmail.com>
2025-10-22 11:38:22 +0300

    Make dir .cache before docker build

--- automation/env/build-images.sh
+++ automation/env/build-images.sh
@@ -12,6 +12,7 @@ popd
 # echo "      Build Hadoop 3.3.6 image      "
 # echo "===================================="
 # pushd hadoop
+# mkdir -p .cache
 # docker build -f Dockerfile -t greengagedb/pxf-hadoop:3.3.6 .
 # popd
 
@@ -25,5 +26,6 @@ echo "Build PXF image for automation"
 echo "=============================="
 # GGDB_IMAGE is from https://github.com/GreengageDB/greengage/tree/main/ci
 pushd ../..
+mkdir -p .cache
 docker build -t greengagedb/ggdb6_pxf_automation --build-arg "GGDB_IMAGE=${GGDB_IMAGE:-greengagedb/ggdb6_ubuntu:6.29.1}" -f automation/env/Dockerfile .
 popd
