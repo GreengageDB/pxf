8617ca8a76806b57870d92434bd2854f3d191051
Maxim Gajdaj <maxim.gajdaj@gmail.com>
2025-11-07 09:20:29 +0300

    upd(automation/env/compose.sh): Quote SCRIPT_DIR variable

--- automation/env/it.sh
+++ automation/env/it.sh
@@ -76,7 +76,7 @@ fi
 export TYPE=$([ -n "$USE_FDW" ] && echo -n "fdw" || echo -n "external-table")
 echo -en "-----\n----- Start running '$GROUP' tests with $TYPE\n-----\n"
 
-bash $SCRIPT_DIR/compose.sh up
+bash "$SCRIPT_DIR"/compose.sh up
 
 [ -n "$DEBUG" ] && echo "Run 'docker compose exec \"$TEST_SERVICE\" sudo -H -u gpadmin bash -l -c \"make -C \$TEST_HOME GROUP=$GROUP USE_FDW=$USE_FDW\"'" | tee -a "$DEBUG_DIR/compose_before_down.log" || true
 docker compose exec "$TEST_SERVICE" sudo -H -u gpadmin bash -l -c "make -C \$TEST_HOME GROUP=$GROUP USE_FDW=$USE_FDW"
