ca409a2bd4ced015f3279552714fe8821fe9c241
Denis Kovalev <d.kovalev@arenadata.io>
2026-06-01 16:44:29 +0300

    Unify tests for 6 and 7

--- fdw/isolation2/sql/pxf_fdw_metadata.sql
+++ fdw/isolation2/sql/pxf_fdw_metadata.sql
@@ -1,3 +1,9 @@
+-- Check if metatata is supported in this version of Greengage (6.31+)
+with version as (
+    select string_to_array(substring(version(), '(?:Database\s)(\d+\.\d+)(?:\.\d+)'), '.') as ver
+)
+select ver[1]::int = 6 and ver[2]::int >= 31 from version;
+
 CREATE EXTENSION IF NOT EXISTS pxf_fdw;
 
 2:!& python3 pxf_mock.py > /tmp/pxf_mock.log 2>&1;
