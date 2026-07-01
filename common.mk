08d49d4070939187b63c0186e883c7c851a89e9b
dkhomutov <d.khomutov@arenada.io>
2026-07-01 19:14:11 +0300

    ADBDEV-10027: stop building pxf when prepare docker images for integration tests

--- common.mk
+++ common.mk
@@ -2,10 +2,5 @@ SHELL := /bin/bash
 # Undefine MAKEOVERRIDES to prevent Spring Boot expression issues
 override undefine MAKEOVERRIDES
 
-# Fail early if GPHOME not found
-ifeq ($(GPHOME),)
-$(error GPHOME not defined)
-endif
-
 # Default PXF_HOME
 PXF_HOME ?= $(GPHOME)/pxf
